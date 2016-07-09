101cfc2f8d971bbb4bab1334ff722205ac9c2f71
Yami Odymel <yamiodymel@gmail.com>
2016-07-09 01:51:33 +0000

    Added resolve test.

--- test/YunomiTest.php
+++ test/YunomiTest.php
@@ -10,6 +10,24 @@ class A
     }
 }
 
+class B
+{
+    function __construct($data = null)
+    {
+        if($data !== null)
+            $this->data = $data;
+    }
+}
+
+class C
+{
+    function __construct(A $a, B $b)
+    {
+        $this->a = $a;
+        $this->b = $b;
+    }
+}
+
 class YunomiTest extends \PHPUnit_Framework_TestCase
 {
     function testBasic()
@@ -28,6 +46,16 @@ class YunomiTest extends \PHPUnit_Framework_TestCase
         $A = Yunomi::get('A');
         
         $this->assertEquals($A, new A('A'));
+    }
+    
+    function testResolve()
+    {
+        $C = Yunomi::inject('A', 'B', function($a, $b)
+        {
+            return new C($a, $b);
+        });
+        
+        $this->assertEquals($C, new C(new A(), new B()));
     }
 }
 ?>
