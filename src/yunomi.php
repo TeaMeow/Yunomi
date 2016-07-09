7473d3809b3e527ff85a9205e37dbbc682ae850e
Yami Odymel <yamiodymel@gmail.com>
2016-07-09 00:10:28 +0000

    First commit.

---
+++ src/yunomi.php
@@ -0,0 +1,60 @@
+<?php
+class Yunomi
+{
+    static $map = [];
+    
+    static function register($name, $class, $args = null)
+    {
+        self::$map[$name] = [$class, $args];
+    }
+    
+    static function get($name)
+    {
+        list($class, $args) = isset(self::$map[$name]) ? self::$map[$name] 
+                                                       : [$name, null];
+                                                       
+        if(class_exists($class, true))
+        {
+            $reflectionClass = new ReflectionClass($class);
+            
+            return !empty($args) ? $reflectionClass->newInstanceArgs($args) 
+                                 : new $class();
+        }
+
+        return null;
+    }
+    
+    static function inject()
+    {
+        $args       = func_get_args();
+        $callback   = array_pop($args);
+        $injectArgs = [];
+
+        foreach($args as $name)
+            $injectArgs[] = self::get($name);
+
+        return call_user_func_array($callback, $injectArgs);
+    }
+    
+    static function resolve($name)
+    {
+        if(!class_exists($name, true))
+            return null;
+
+        $reflectionClass       = new ReflectionClass($name);
+        $reflectionConstructor = $reflectionClass->getConstructor();
+        $reflectionParams      = $reflectionConstructor->getParameters();
+
+        $args = [];
+        
+        foreach($reflectionParams as $param)
+        {
+            $class  = $param->getClass()->getName();
+            $args[] = self::get($class);
+        }
+
+        return !empty($args) ? $reflectionClass->newInstanceArgs($args) 
+                             : new $class();
+    }
+}
+?>
