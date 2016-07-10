012373000f2c02f1df4c5aac22091e4535f94ce5
Yami Odymel <yamiodymel@gmail.com>
2016-07-10 18:09:32 +0000

    get() can now return multiple classes

--- src/yunomi.php
+++ src/yunomi.php
@@ -3,32 +3,46 @@
 /**
  * http://jaceju.net/2014-07-27-php-di-container/
  */
- 
+
 class Yunomi
 {
     static $map = [];
-    
+
     static function register($name, $class, $args = null)
     {
         self::$map[$name] = [$class, $args];
     }
-    
+
     static function get($name)
     {
-        list($class, $args) = isset(self::$map[$name]) ? self::$map[$name] 
+        if(is_array($name))
+        {
+            $classes = [];
+
+            foreach($name as $single)
+               $classes[] = self::implement($single);
+
+            return $classes;
+        }
+
+        return self::implement($name);
+    }
+
+    static function implement($name)
+    {
+        list($class, $args) = isset(self::$map[$name]) ? self::$map[$name]
                                                        : [$name, null];
-                                                       
+
         if(class_exists($class, true))
         {
             $reflectionClass = new ReflectionClass($class);
-            
-            return !empty($args) ? $reflectionClass->newInstanceArgs($args) 
+
+            return !empty($args) ? $reflectionClass->newInstanceArgs($args)
                                  : new $class();
         }
-
-        return null;
     }
-    
+
+
     static function inject()
     {
         $args       = func_get_args();
@@ -40,7 +54,7 @@ class Yunomi
 
         return call_user_func_array($callback, $injectArgs);
     }
-    
+
     static function resolve($name)
     {
         if(!class_exists($name, true))
@@ -51,14 +65,14 @@ class Yunomi
         $reflectionParams      = $reflectionConstructor->getParameters();
 
         $args = [];
-        
+
         foreach($reflectionParams as $param)
         {
             $class  = $param->getClass()->getName();
             $args[] = self::get($class);
         }
 
-        return !empty($args) ? $reflectionClass->newInstanceArgs($args) 
+        return !empty($args) ? $reflectionClass->newInstanceArgs($args)
                              : new $class();
     }
 }
--- test/YunomiTest.php
+++ test/YunomiTest.php
@@ -33,42 +33,51 @@ class YunomiTest extends \PHPUnit_Framework_TestCase
     function testBasic()
     {
         Yunomi::register('A', 'A');
-        
+
         $A = Yunomi::get('A');
-        
+
         $this->assertEquals($A, new A());
     }
-    
+
+    function testMultipleBasic()
+    {
+        Yunomi::register('A', 'A');
+
+        $A = Yunomi::get(['A', 'A']);
+
+        $this->assertEquals($A, [new A(), new A()]);
+    }
+
     function testNone()
     {
         $D = Yunomi::get('D');
-        
+
         $this->assertEquals($D, null);
     }
-    
+
     function testBasicWithArg()
     {
         Yunomi::register('A', 'A', ['A']);
-        
+
         $A = Yunomi::get('A');
-        
+
         $this->assertEquals($A, new A('A'));
     }
-    
+
     function testInject()
     {
         $C = Yunomi::inject('A', 'B', function($a, $b)
         {
             return new C($a, $b);
         });
-        
+
         $this->assertEquals($C, new C(new A('A'), new B()));
     }
-    
+
     function testResolve()
     {
         $C = Yunomi::resolve('C');
-        
+
         $this->assertEquals($C, new C(new A('A'), new B()));
     }
 }
